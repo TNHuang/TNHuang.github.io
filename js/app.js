fe616e34df031f08f35b4bfa23759403cc587650
Kenny Huang <KHuangtai@gmail.com>
2015-01-16 19:15:17 -0500

    re order mpn to front of table

--- js/app.js
+++ js/app.js
@@ -1,23 +1,5 @@
 window.onload = function(){
 	var query = document.getElementById("query");
-	var submit = document.getElementsByClassName("submit-btn")[0];
-	var qresult = document.getElementsByClassName("query-results")[0];
-	
-    // var xmlhttp;
-
-    // if (window.XMLHttpRequest){//IE7+, firefox, chrome, opera, safari
-    // 	xmlhttp = new XMLHttpRequest();
-    // }else{
-    // 	xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    // }
-
-    // submit.addEventListener("click", setSrc);
-
-    // function setSrc(){
-    
-    // 	scriptTag.src = url + "&q=" + query.value.replace(/\s/g, "+");
-    // 	result.innerHTML = query.value;
-    // }
 
 	query.addEventListener("keypress", function(){
 
@@ -30,7 +12,7 @@ window.onload = function(){
 			url += "?apikey=612b8697";
 			url += "&callback=logIt";
 
-			qurl =  url + "&q=" + query.value.replace(/\s/g, "+") + "&start=0&limit=20";
+			qurl =  url + "&q=" + query.value.replace(/\s/g, "+") + "&start=0&limit=10";
 			scriptTag.src = qurl;
 
 		}, 0)
